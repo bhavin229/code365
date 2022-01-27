2c6d18106177802e4446fa2ecb76eb3160da4b79
bhavin229 <vasoyakhimji@gmail.com>
2022-01-27 06:04:00 +0000

    largest number at least twice of others

---
+++ largest number at least twice of others.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        const auto result = distance(nums.begin(), max_element(nums.begin(),nums.end()));
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i != result && 2 * nums[i] > nums[result]) {
+                return -1;
+            }
+        }
+        return result;
+    }
+};
