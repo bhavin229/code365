db753ed85366f18a6e4f6c0941922205e98fab4e
bhavin229 <vasoyakhimji@gmail.com>
2022-03-10 02:52:00 +0000

    divide array in sets of k consecutive numbers

---
+++ divide array in sets of k consecutive numbers.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& nums, int k) {
+        map<int, int> count;
+        for (const auto& num : nums) {
+            ++count[num];
+        }
+        for (const auto [num, c] : count) {
+            if (!c) {
+                continue;
+            }
+            for (int i = num; i < num + k; ++i) {
+                if (count[i] < c) {
+                    return false;
+                }
+                count[i] -= c;
+            }
+        }
+        return true;
+    }
+};
