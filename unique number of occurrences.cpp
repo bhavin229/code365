79ebac5b3154111c6f39835c0d58110fc8c065b6
bhavin229 <vasoyakhimji@gmail.com>
2022-03-13 03:50:00 +0000

    unique number of occurrences

---
+++ unique number of occurrences.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> count;
+        for (const auto& a : arr) {
+            ++count[a];
+        }
+        unordered_set<int> lookup;
+        for (const auto& [k, v] : count) {
+            if (lookup.count(v)) {
+                return false;
+            }
+            lookup.emplace(v);
+        }
+        return true;
+    }
+};
