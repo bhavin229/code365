c3aac962aaa0bf17602b6e8d67979cdb6843928a
bhavin229 <vasoyakhimji@gmail.com>
2022-04-10 03:26:00 +0000

    house robber

---
+++ house robber.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int last = 0, result = 0;
+        for (const auto& i : nums) {
+            auto tmp = result;
+            result = max(last + i, result);
+            last = tmp;
+        }
+        return result;
+    }
+};
