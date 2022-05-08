82c82f9a15f37579fdcb4de0ef363478e4d6be43
bhavin229 <vasoyakhimji@gmail.com>
2022-05-08 05:39:00 +0000

    thousand separator

---
+++ thousand separator.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    string thousandSeparator(int n) {
+        string result;
+        const auto& s = to_string(n);
+        for (int i = 0; i < s.length(); ++i) {
+            if (i > 0 && (s.length() - i) % 3 == 0) {
+                result += ".";
+            }
+            result += s[i];
+        }
+        return result;
+    }
+};
