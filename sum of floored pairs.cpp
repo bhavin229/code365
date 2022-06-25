a8ab3180fc340e6025549201475d79b1fe3a6638
bhavin229 <vasoyakhimji@gmail.com>
2022-06-25 05:00:00 +0000

    sum of floored pairs

---
+++ sum of floored pairs.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    int sumOfFlooredPairs(vector<int>& nums) {
+        static const int MOD = 1e9 + 7;
+        vector<int> prefix(*max_element(cbegin(nums), cend(nums)) + 1);
+        unordered_map<int, int> count;
+        for (const auto& num : nums) {
+            ++count[num];
+        }
+        for (const auto& [num, cnt] : count) {
+            for (int j = num; j < size(prefix); j += num) {
+                prefix[j] += cnt;
+            }
+        }
+        for (int i = 0; i + 1 < size(prefix); ++i) {
+            prefix[i + 1] += prefix[i];
+        }
+        return accumulate(cbegin(nums), cend(nums), 0,
+                          [&prefix](int total, int x) {
+                              return (total + prefix[x]) % MOD;
+                          });
+    }
+};
