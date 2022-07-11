e3d46c40fbc6569281b1f51354aef1c5bb5cc2da
bhavin229 <vasoyakhimji@gmail.com>
2022-07-11 03:53:00 +0000

    teemo attacking

---
+++ teemo attacking.cpp
@@ -0,0 +1,11 @@
+
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int result = duration * timeSeries.size();
+        for (int i = 1; i < timeSeries.size(); ++i){
+            result -= max(0, duration - (timeSeries[i] - timeSeries[i - 1]));
+        }
+        return result;
+    }
+};
